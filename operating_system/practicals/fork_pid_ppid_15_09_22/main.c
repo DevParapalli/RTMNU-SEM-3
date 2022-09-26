ea60f57e40eca22ded2eef1dee1f3fc0a2997ee3
Devansh Parapalli <parapallidev@gmail.com>
2022-09-26 23:27:26 +0530

    os : refactored fork_pid_ppid.

--- operating_system/practicals/fork_pid_ppid_15_09_22/main.c
+++ operating_system/practicals/fork_pid_ppid_15_09_22/main.c
@@ -2,22 +2,35 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-// Doesn't run on Windows
+void parent_main() {
+  printf("[Parent] \n");
+  unsigned pid = getpid();
+  printf("PID: %u |", pid);
+  unsigned ppid = getppid();
+  printf("PPID: %u \n", ppid);
+}
+
+void child_main() {
+  printf("[Child] \n");
+  unsigned pid = getpid();
+  printf("PID: %u |", pid);
+  unsigned ppid = getppid();
+  printf("PPID: %u \n", ppid);
+}
 
 int main() {
-    printf("[Original] ");
-    unsigned pid = getpid();
-    printf("PID: %u |", pid);
-    unsigned ppid = getppid();
-    printf("PPID: %u \n", ppid);
+  int pid;
 
-    fork();
+  pid = fork();
 
-    printf("[Forking] ");
-    unsigned pid_2 = getpid();
-    printf("PID: %u |", pid_2);
-    unsigned ppid_2 = getppid();
-    printf("PPID: %u \n", ppid_2);
-
+  if (pid == -1) {
+    printf("Creation of child process is unsuccessful, Exiting...");
     return 0;
+  } else if (pid == 0) {
+    child_main();
+  } else {
+    parent_main();
+  }
+
+  return 0;
 }
